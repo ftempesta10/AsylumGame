53622ff8b959176caec881714cf999bca004415d
claudiovalen <claudiovalenziano99@gmail.com>
2020-08-26 19:32:48 +0200

    Aggiunti inventario e comandi

---
+++ namenotfound/src/main/java/avventura/engine/Inventory.java
@@ -0,0 +1,25 @@
+package avventura.engine;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Inventory {
+	
+    private List<AdvObject> list = new ArrayList<>();
+
+    public List<AdvObject> getList() {
+        return list;
+    }
+
+    public void setList(List<AdvObject> list) {
+        this.list = list;
+    }
+
+    public void add(AdvObject o) {
+        list.add(o);
+    }
+
+    public void remove(AdvObject o) {
+        list.remove(o);
+    }
+}
