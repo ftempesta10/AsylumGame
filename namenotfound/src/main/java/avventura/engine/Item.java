c5ba6541d39bca952891039ad341d3d8f26bc585
AlessandroPaparella <alexpapar@hotmail.it>
2020-08-26 16:32:10 +0200

    Aggiunti personaggi, nemici, oggetti

---
+++ namenotfound/src/main/java/avventura/engine/Item.java
@@ -0,0 +1,74 @@
+package engine;
+
+import java.io.Serializable;
+import java.util.Set;
+
+public class Item implements Serializable {
+	private static Integer istances;
+	private String name, description;
+	private Integer id;
+	private Set<String> alias;
+	private Boolean pushed, opened;
+	private Set<CommandType> commands;
+
+	public Item(String name, String description) {
+		super();
+		this.name = name;
+		this.description = description;
+		id = istances;
+		istances++;
+		pushed=false;
+		opened=false;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Set<String> getAlias() {
+		return alias;
+	}
+
+	public void setAlias(Set<String> alias) {
+		this.alias = alias;
+	}
+
+	public Boolean isPushed() {
+		return pushed;
+	}
+
+	public void setPushed(Boolean pushed) {
+		this.pushed = pushed;
+	}
+
+	public Boolean isOpened() {
+		return opened;
+	}
+
+	public void setOpened(Boolean opened) {
+		this.opened = opened;
+	}
+
+	public Set<CommandType> getCommands() {
+		return commands;
+	}
+
+	public void setCommands(Set<CommandType> commands) {
+		this.commands = commands;
+	}
+
+
+
+}
