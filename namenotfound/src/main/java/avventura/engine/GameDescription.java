7bf9d3cfcde00a6daedf46d1dfb8b98ed864ae3a
ftempesta11 <felicetempesta30@gmail.com>
2020-08-30 16:05:17 +0200

    Implementata classe GameDescription

--- namenotfound/src/main/java/avventura/engine/GameDescription.java
+++ namenotfound/src/main/java/avventura/engine/GameDescription.java
@@ -1,5 +1,33 @@
 package engine;
 
-public class GameDescription {
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import hashedGraph.WeightedHashedGraph;
+
+public abstract class GameDescription<T, W> {
+
+    private Room currentRoom;
+
+    private WeightedHashedGraph<T, W> map;
+     
+    private final List<Item> inventory = new ArrayList<Item>();
+
+    public Room getCurrentRoom() {
+        return currentRoom;
+    }
+
+    public void setCurrentRoom(Room currentRoom) {
+        this.currentRoom = currentRoom;
+    }
+
+    public List<Item> getInventory() {
+        return inventory;
+    }
+
+    public abstract void init() throws Exception;
+
+    public abstract void nextMove(ParserOutput p, PrintStream out);
 
 }
