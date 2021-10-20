0c6871847b9845e2eb1a5d75d56a9cc9538b3142
AlessandroPaparella <alexpapar@hotmail.it>
2021-10-20 15:18:55 +0200

    Operatore ugualianza aggiunta id

--- namenotfound/src/main/java/avventura/engine/Item.java
+++ namenotfound/src/main/java/avventura/engine/Item.java
@@ -93,6 +93,7 @@ public class Item implements Serializable {
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		result = prime * result + ((name == null) ? 0 : name.hashCode());
 		return result;
 	}
@@ -106,6 +107,11 @@ public class Item implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		Item other = (Item) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
 		if (name == null) {
 			if (other.name != null)
 				return false;
@@ -113,6 +119,8 @@ public class Item implements Serializable {
 			return false;
 		return true;
 	}
+
+
 
 
 
